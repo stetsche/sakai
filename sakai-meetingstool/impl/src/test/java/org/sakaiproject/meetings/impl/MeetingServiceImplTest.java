c83f8e4288ce8b9f07e5c73f459fbcc9056b17b1
ropemar <ropemar@gmail.com>
2022-04-04 11:38:22 +0200

    SAK-45116 Add test source folder to Impl.

---
+++ sakai-meetingstool/impl/src/test/java/org/sakaiproject/meetings/impl/MeetingServiceImplTest.java
@@ -0,0 +1,13 @@
+package org.sakaiproject.meetings.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class MeetingServiceImplTest {
+
+    @Test
+    public void test_Add() {
+      System.out.println("Test!!!!");
+    }
+    
+}
