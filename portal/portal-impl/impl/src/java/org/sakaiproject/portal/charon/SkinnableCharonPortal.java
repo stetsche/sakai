5f4853fc38658e5ad7de6a341f789b8a6b8b28c6
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2022-07-28 10:23:32 +0200

    TRINITY-85 Implement new footer
    Adds new footer markup
    Improved configuration
    Adds new script for serverinfo popover
    
    Co-authored-by: Sandro Kopp <sandrokopp90@gmail.com>

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -22,8 +22,11 @@ package org.sakaiproject.portal.charon;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.text.Collator;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Enumeration;
@@ -33,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TimeZone;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -1601,8 +1605,9 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			Preferences prefs = preferencesService.getPreferences(thisUser);
 
 			boolean showServerTime = ServerConfigurationService.getBoolean("portal.show.time", true);
+			rcontext.put("showServerTime", showServerTime);
+
 			if (showServerTime) {
-					rcontext.put("showServerTime","true");
 					Calendar now = Calendar.getInstance();
 					Date nowDate = new Date(now.getTimeInMillis());
 
@@ -1637,7 +1642,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 
 							now.setTimeZone(preferredTz);
 
-							rcontext.put("showPreferredTzTime", "true");
+							rcontext.put("showPreferredTzTime", true);
 
 							//now set up the portal information
 							rcontext.put("preferredTzDisplay",
@@ -1653,14 +1658,13 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 													)
 											);
 					} else {
-							rcontext.put("showPreferredTzTime", "false");
+							rcontext.put("showPreferredTzTime", false);
 					}
 			}
 			
 			rcontext.put("pagepopup", false);
 
-			String copyright = ServerConfigurationService
-			.getString("bottom.copyrighttext");
+			String copyright = ServerConfigurationService.getString("bottom.copyrighttext");
 
 			/**
 			 * Replace keyword in copyright message from sakai.properties 
@@ -1673,86 +1677,86 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			String service = ServerConfigurationService.getString("ui.service", "Sakai");
 			String serviceVersion = ServerConfigurationService.getString(
 					"version.service", "?");
-			String sakaiVersion = ServerConfigurationService.getString("version.sakai",
-			"?");
+			String sakaiVersion = ServerConfigurationService.getString("version.sakai", "?");
 			String server = ServerConfigurationService.getServerId();
-			String[] bottomNav = ServerConfigurationService.getStrings("bottomnav");
+			String[] footerLinks = ServerConfigurationService.getStrings("footerlinks");
+			List<HashMap<String, String>> footerLinksList = Arrays.asList(footerLinks).stream()
+					.map((footerlink) -> {
+						String[] linkValues = StringUtils.split(footerlink, ";");
+						if (linkValues.length < 2) {
+							return null;
+						} else {
+							HashMap<String, String> linkMap = new HashMap();
+							linkMap.put("text", linkValues[0]);
+							linkMap.put("href", linkValues[1]);
+							if (linkValues.length >= 3) {
+								linkMap.put("target", linkValues[2]);
+							}
+							return linkMap;
+						}
+					})
+				.filter((link) -> link != null)
+			.collect(Collectors.toList());
+			rcontext.put("footerLinks", footerLinksList);
 			String[] poweredByUrl = ServerConfigurationService.getStrings("powered.url");
-			String[] poweredByImage = ServerConfigurationService
-			.getStrings("powered.img");
-			String[] poweredByAltText = ServerConfigurationService
-			.getStrings("powered.alt");
+			String[] poweredByImage = ServerConfigurationService.getStrings("powered.img");
+			String[] poweredByAltText = ServerConfigurationService.getStrings("powered.alt");
 
-			{
-				List<Object> l = new ArrayList<Object>();
-				if ((bottomNav != null) && (bottomNav.length > 0))
-				{
-					for (int i = 0; i < bottomNav.length; i++)
-					{
-						l.add(bottomNav[i]);
+			String neoChatProperty = ServerConfigurationService.getString(Site.PROP_SITE_PORTAL_NEOCHAT, "never");
+			boolean neoChatAvailable = false;
+
+			if ("true".equals(neoChatProperty) || "false".equals(neoChatProperty)) {
+				neoChatAvailable = Boolean.valueOf(neoChatProperty);
+				if (site != null) {
+					String siteNeoChatStr = site.getProperties().getProperty(Site.PROP_SITE_PORTAL_NEOCHAT);
+					if (siteNeoChatStr != null) {
+						neoChatAvailable = Boolean.valueOf(siteNeoChatStr);
 					}
 				}
-				rcontext.put("bottomNav", l);
 			}
 
-                        String neoChatProperty = ServerConfigurationService.getString(Site.PROP_SITE_PORTAL_NEOCHAT, "never");
-                        boolean neoChatAvailable = false;
- 
-                        if ("true".equals(neoChatProperty) || "false".equals(neoChatProperty)) {
-                            neoChatAvailable = Boolean.valueOf(neoChatProperty);
-                            if (site != null) {
-                                String siteNeoChatStr = site.getProperties().getProperty(Site.PROP_SITE_PORTAL_NEOCHAT);
-                                if (siteNeoChatStr != null) {
-                                    neoChatAvailable = Boolean.valueOf(siteNeoChatStr);
-                                }
-                            }
-                        }
+			if ("always".equals(neoChatProperty)) {
+				neoChatAvailable = true;
+			}
 
-                        if ("always".equals(neoChatProperty)) {
-                            neoChatAvailable = true;
-                        }
+			if (!chatHelper.checkChatPermitted(thisUser)) {
+				neoChatAvailable = false;
+			}
 
-                        if (!chatHelper.checkChatPermitted(thisUser)) {
-                            neoChatAvailable = false;
-                        }
+			rcontext.put("neoChat", neoChatAvailable);
+			rcontext.put("portalChatPollInterval", ServerConfigurationService.getInt("portal.chat.pollInterval", 5000));
+			rcontext.put("neoAvatar", ServerConfigurationService.getBoolean("portal.neoavatar", true));
+			rcontext.put("neoChatVideo", ServerConfigurationService.getBoolean("portal.chat.video", true));
+			rcontext.put("portalVideoChatTimeout", ServerConfigurationService.getInt("portal.chat.video.timeout", 25));
 
-                        rcontext.put("neoChat", neoChatAvailable);
-                        rcontext.put("portalChatPollInterval", 
-				ServerConfigurationService.getInt("portal.chat.pollInterval", 5000));
-                        rcontext.put("neoAvatar", 
-				ServerConfigurationService.getBoolean("portal.neoavatar", true));
-                        rcontext.put("neoChatVideo", 
-				ServerConfigurationService.getBoolean("portal.chat.video", true));
-                        rcontext.put("portalVideoChatTimeout", 
-				ServerConfigurationService.getInt("portal.chat.video.timeout", 25));
+			if (sakaiTutorialEnabled && thisUser != null) {
+				if (!("1".equals(prefs.getProperties().getProperty("sakaiTutorialFlag")))) {
+					rcontext.put("tutorial", true);
+					//now save this in the user's preferences so we don't show it again
+					PreferencesEdit preferences = null;
+					try {
+						preferences = preferencesService.edit(thisUser);
+						ResourcePropertiesEdit props = preferences.getPropertiesEdit();
+						props.addProperty("sakaiTutorialFlag", "1");
+						preferencesService.commit(preferences);   
+					} catch (SakaiException e1) {
+						log.error(e1.getMessage(), e1);
+					}
+				}
+			}
 
-                        if(sakaiTutorialEnabled && thisUser != null) {
-                        	if (!("1".equals(prefs.getProperties().getProperty("sakaiTutorialFlag")))) {
-                        		rcontext.put("tutorial", true);
-                        		//now save this in the user's preferences so we don't show it again
-                        		PreferencesEdit preferences = null;
-                        		try {
-                        			preferences = preferencesService.edit(thisUser);
-                        			ResourcePropertiesEdit props = preferences.getPropertiesEdit();
-                        			props.addProperty("sakaiTutorialFlag", "1");
-                        			preferencesService.commit(preferences);   
-                        		} catch (SakaiException e1) {
-                        			log.error(e1.getMessage(), e1);
-                        		}
-                        	}
-                        }
-
-			if(sakaiThemesEnabled) {
+			if (sakaiThemesEnabled) {
 				rcontext.put("sakaiThemesEnabled", true);
 
-				if(sakaiThemeSwitcherEnabled) {
+				if (sakaiThemeSwitcherEnabled) {
 					rcontext.put("themeSwitcher", true);
 				}
 				
-				if(sakaiThemesAutoDetectDarkEnabled) {
+				if (sakaiThemesAutoDetectDarkEnabled) {
 					rcontext.put("themesAutoDetectDark", true);
 				}
-				String userTheme = StringUtils.defaultIfEmpty(prefs.getProperties(org.sakaiproject.user.api.PreferencesService.USER_SELECTED_UI_THEME_PREFS).getProperty("theme"), "sakaiUserTheme-notSet");
+				String userTheme = StringUtils.defaultIfEmpty(prefs.getProperties(org.sakaiproject.user.api.PreferencesService.USER_SELECTED_UI_THEME_PREFS)
+					.getProperty("theme"), "sakaiUserTheme-notSet");
 				rcontext.put("userTheme", userTheme);
 			}
 
